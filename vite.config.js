8caca1bc1571535e9dbc7f5f1cf166eabd628852
Lenovo <3153566913@qq.com>
2023-05-10 21:08:08 +0800

    主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,23 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      //配置elementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({importStyle:"sass"})],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
