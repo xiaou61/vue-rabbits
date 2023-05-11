7d802b1901f4dd3ec018b9f86c4c81b6087157a3
Lenovo <3153566913@qq.com>
2023-05-11 20:38:37 +0800

    home-新鲜好物和人气推荐完成

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,23 @@ export function getBannerAPI(){
         url : "/home/banner"
     })
 }
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+        url:'/home/new'
+    })
+}
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+    return  httpInstance({
+        url : 'home/hot'
+    } )
+}
