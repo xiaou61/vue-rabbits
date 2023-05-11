c34830b65bf7f919c37640c90085fbd13b1e4c60
Lenovo <3153566913@qq.com>
2023-05-11 17:18:21 +0800

    Layout渲染一级列表

---
+++ src/apis/layout.js
@@ -0,0 +1,6 @@
+import httpInstance from "@/utils/http";
+export function getCategoryAPI(){
+    return httpInstance({
+        url : "/home/category/head"
+    })
+}
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,4 +1,22 @@
+
 <script setup>
+import {getCategoryAPI} from "@/apis/layout";
+import {onMounted, ref} from "vue";
+
+const categoryList=ref([])
+
+const getCategory = async () => {
+
+  const res= await getCategoryAPI()
+  console.log(res)
+  categoryList.value=res.result
+}
+
+
+
+onMounted(()=>{
+  getCategory()
+})
 
 </script>
 
@@ -9,17 +27,8 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">居家</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">美食</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">服饰</RouterLink>
+        <li class="home" v-for="item in categoryList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
       </ul>
       <div class="search">
