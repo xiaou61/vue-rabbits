713195b03b797aef8b047c5602cd8db68b1b993a
Lenovo <3153566913@qq.com>
2023-05-11 17:44:53 +0800

    layout-吸顶导航交互事件

--- src/main.js
+++ src/main.js
@@ -8,12 +8,6 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 
-//测试接口函数
-import {getCategory} from "@/apis/testAPI";
-getCategory().then(res=>{
-    console.log(res)
-})
-
 const app = createApp(App)
 
 app.use(createPinia())
