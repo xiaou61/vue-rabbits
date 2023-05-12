8fd8834c83180cfae6ca03656a70a2ab46da762e
Lenovo <3153566913@qq.com>
2023-05-12 17:58:12 +0800

    二级分类--无限加载功能

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -39,6 +39,19 @@ const tabchange = () => {
   getGoodList()
 }
 
+
+//加载更多
+const disabled=ref(false)
+const load = async () => {
+  //获得下一页的数据
+  reqData.value.page++
+  const res=await getSubCategoryAPI(reqData.value)
+  goodList.value=[...goodList.value,...res.result.items]
+  //加载完毕，停止监听
+  if (res.result.items.lens===0){
+    disabled.value=true
+  }
+}
 </script>
 
 <template>
@@ -58,7 +71,7 @@ const tabchange = () => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
