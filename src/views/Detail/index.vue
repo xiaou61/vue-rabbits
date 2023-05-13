c7bec1d486e197af4b88a856dc55cae0e47b7ec6
Lenovo <3153566913@qq.com>
2023-05-13 16:29:09 +0800

    详情页--热榜基础组件封装，和基础数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import {getDetail} from "@/apis/detail";
 import {onMounted, ref} from "vue";
 import {useRoute} from "vue-router";
+import DetailHot from "@/views/Detail/components/DetailHot.vue";
 const route=useRoute()
 const goods=ref({})
 const getGoods = async () => {
@@ -119,6 +120,10 @@ onMounted(()=>getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
+<!--              24小时-->
+              <DetailHot/>
+<!--              周-->
+              <DetailHot/>
 
             </div>
           </div>
