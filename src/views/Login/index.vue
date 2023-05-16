22ee0e1aa4eee47c1ff0d44679c6492c192faa36
Lenovo <3153566913@qq.com>
2023-05-16 21:08:54 +0800

    登录--表单校验--统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -33,6 +33,22 @@ const rules = {
   ]
 }
 
+
+//获得form实例
+const formRef=ref(null)
+const doLogin = () => {
+  //调用实例方法
+  formRef.value.validate((valid)=>{
+    //valid所有表单都通过检验，才是true
+    //以valid作为判断条件 如果通过校验，才执行登录逻辑
+    if (valid){
+      //去登录
+    }
+  })
+}
+
+
+
 </script>
 
 
@@ -57,7 +73,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
                      status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -70,7 +86,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
