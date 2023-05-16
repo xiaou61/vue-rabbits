c261dc1152b0faba05748b3d2a00afa391491198
Lenovo <3153566913@qq.com>
2023-05-16 21:00:43 +0800

    登录--表单校验--自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -6,7 +6,8 @@ import {ref} from "vue";
 
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree:true
 })
 //准备规则对象
 const rules = {
@@ -16,6 +17,19 @@ const rules = {
   password: [
     {required: true, message: '密码不能为空', trigger: 'blur'},
     {min: 6, max: 14, message: '密码长度为6~14个字符', trigger: 'blur'}
+  ],
+  agree:[
+    {
+     validator:(rule,value,callback)=>{
+       //自定义校验逻辑
+       //勾选就通过，不勾选就不通过
+       if (value){
+         callback()
+       }else {
+         callback(new Error("请勾选协议"))
+       }
+     }
+    }
   ]
 }
 
@@ -51,8 +65,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
