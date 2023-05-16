fe06171a8dc5a48948d191606ea5472791eea65c
Lenovo <3153566913@qq.com>
2023-05-16 17:00:55 +0800

    SKU组件的认识

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import {onMounted, ref} from "vue";
 import {useRoute} from "vue-router";
 import DetailHot from "@/views/Detail/components/DetailHot.vue";
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from "@/components/XtxSku/index.vue";
 const route=useRoute()
 const goods=ref({})
 const getGoods = async () => {
@@ -11,6 +12,11 @@ const getGoods = async () => {
   goods.value = res.result
 }
 onMounted(()=>getGoods())
+
+
+const skuChange = (sku) => {
+  console.log(sku)
+}
 
 </script>
 
@@ -87,7 +93,7 @@ onMounted(()=>getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+                <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
