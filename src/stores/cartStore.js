a7f24160377edbee7c42eddbe6af49e56d04e827
Lenovo <3153566913@qq.com>
2023-05-17 17:12:31 +0800

    本地购物车--头部购物车--计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,36 +1,48 @@
 //封装购物车模块
 import {defineStore} from "pinia";
-import {ref} from "vue";
-export const useCartStore=defineStore('cart',()=>{
-    //定义state
-    const cartList=ref([])
-    //定义action
-    const addCart = (goods) => {
-        //通过匹配传递过来的商品对象中的skuid能不能再cartlist中找到
-        //添加过 count+1
-        const item=cartList.value.find((item)=>goods.skuId===item.skuId)
-        if (item){
-            //找到了
-            item.count++
-        }else {
-            //没有添加过--直接push
-            cartList.value.push(goods)
+import {computed, ref} from "vue";
+
+export const useCartStore = defineStore('cart', () => {
+        //定义state
+        const cartList = ref([])
+        //定义action
+        const addCart = (goods) => {
+            //通过匹配传递过来的商品对象中的skuid能不能再cartlist中找到
+            //添加过 count+1
+            const item = cartList.value.find((item) => goods.skuId === item.skuId)
+            if (item) {
+                //找到了
+                item.count++
+            } else {
+                //没有添加过--直接push
+                cartList.value.push(goods)
+            }
+
+        }
+        //删除购物车
+        const delCart = (skuId) => {
+            //思路 找到要删除项的下标值--splice
+            //使用数组的过滤方法--filter
+            const index = cartList.value.findIndex((item) => skuId === item.skuId);
+            cartList.value.splice(index, 1)
         }
 
-    }
-    //删除购物车
-    const delCart = (skuId) => {
-      //思路 找到要删除项的下标值--splice
-        //使用数组的过滤方法--filter
-        const index = cartList.value.findIndex((item)=>skuId===item.skuId);
-        cartList.value.splice(index,1)
-    }
-    return{
-        cartList,
-        addCart,
-        delCart
-    }
-},
+
+        //计算属性
+        //a为每一次累加完以后都会交给a  c为每一项
+        //1.总的数量 所有的count和
+        const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+        //2.总价   所有的count*price之和
+        const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
+        return {
+            cartList,
+            addCart,
+            delCart,
+            allCount,
+            allPrice
+        }
+    },
     {
         persist: true
     })
