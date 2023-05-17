c27988547210e222cc2d67a28587e2391f510502
Lenovo <3153566913@qq.com>
2023-05-17 17:20:45 +0800

    本地购物车--列表购物车--基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import Subcategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,10 @@ const router = createRouter({
                 {
                     path: 'detail/:id',
                     component: Detail
+                },
+                {
+                    path: 'cartlist',
+                    component: CartList
                 }
             ]
         },
@@ -41,13 +46,12 @@ const router = createRouter({
         }
     ],
     //路由滚动行为
-    scrollBehavior(){
-        return{
-            top:0
+    scrollBehavior() {
+        return {
+            top: 0
         }
     }
 })
-
 
 
 export default router
