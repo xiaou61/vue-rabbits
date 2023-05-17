a37bf66b08e062ce8f5262fca87131ab65e2bfc8
Lenovo <3153566913@qq.com>
2023-05-17 09:00:12 +0800

    登录--token 401校验

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 import {ElMessage} from "element-plus";
 import 'element-plus/theme-chalk/el-message.css'
 import {useUserStore} from "@/stores/user";
+import Router from "@/router";
 //axios基础的封装
 import axios from "axios";
 
@@ -24,11 +25,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     //统一错误提示
     ElMessage({
         type: 'warning',
         message: e.response.data.message
     })
+    //401token失效处理
+    //清除本地用户数据
+    //跳转到登录页
+    if (e.response.status===401){
+        userStore.clearUserInfo()
+        Router.push('/login')
+    }
     return Promise.reject(e)
 })
 
