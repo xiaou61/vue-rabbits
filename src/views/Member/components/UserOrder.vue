38b76c9f9ef596c9c94ebca76b84aeae19d8fa7b
Lenovo <3153566913@qq.com>
2023-05-26 13:43:23 +0800

    订单--分页逻辑

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -13,6 +13,7 @@ const tabTypes = [
 ]
 // 订单列表
 const orderList = ref([])
+const total=ref(0)
 const params=ref({
   orderState:0,
   page:1,
@@ -22,6 +23,7 @@ const params=ref({
 const getOrderList = async () => {
   const res = await getUserOrder(params.value)
   orderList.value = res.result.items
+  total.value=res.result.counts
 }
 
 
@@ -31,6 +33,12 @@ onMounted(()=>getOrderList())
 //tab切换
 const tabChange= (type) => {
   params.value.orderState=type
+  getOrderList()
+}
+
+//页数切换
+const pageChange = (page) => {
+    params.value.page=page
   getOrderList()
 }
 </script>
@@ -115,7 +123,7 @@ const tabChange= (type) => {
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination :total="total" @current-change="pageChange" :page-size="params.pageSize" background layout="prev, pager, next" />
           </div>
         </div>
       </div>
