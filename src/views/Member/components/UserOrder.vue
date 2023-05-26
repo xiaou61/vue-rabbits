901ad5395beec295057bf2ba39a97ce380aed536
Lenovo <3153566913@qq.com>
2023-05-26 13:24:14 +0800

    订单--tab切换

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -27,11 +27,17 @@ const getOrderList = async () => {
 
 onMounted(()=>getOrderList())
 
+
+//tab切换
+const tabChange= (type) => {
+  params.value.orderState=type
+  getOrderList()
+}
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
