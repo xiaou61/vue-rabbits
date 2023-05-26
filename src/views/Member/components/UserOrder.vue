dadf6525cdb74ef0e7a2fec1cc3bb842a29100a8
Lenovo <3153566913@qq.com>
2023-05-26 13:52:45 +0800

    订单--细节优化

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -41,6 +41,20 @@ const pageChange = (page) => {
     params.value.page=page
   getOrderList()
 }
+
+// 创建格式化函数
+const fomartPayState = (payState) => {
+  const stateMap = {
+    1: '待付款',
+    2: '待发货',
+    3: '待收货',
+    4: '待评价',
+    5: '已完成',
+    6: '已取消'
+  }
+  return stateMap[payState]
+}
+
 </script>
 
 <template>
@@ -86,7 +100,7 @@ const pageChange = (page) => {
                 </ul>
               </div>
               <div class="column state">
-                <p>{{ order.orderState }}</p>
+                <p>{{fomartPayState(order.orderState) }}</p>
                 <p v-if="order.orderState === 3">
                   <a href="javascript:;" class="green">查看物流</a>
                 </p>
